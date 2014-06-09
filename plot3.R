7907d980dbcf9904df7a3c4acc73c371d2461dd4
pinakmishra <bhusan.pinak@gmail.com>
2014-06-09 07:19:47 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,24 +1,25 @@
-#plot2
-#histogram on the Global active power
-hpc <- read.csv("household_power_consumption.txt", sep=";")
-
-#converts the date formating to dd/mm/yyy
-hpc$Date <- as.Date(hpc$Date , "%d/%m/%Y")
-
-#get the global active power column as numeric for histogram
-hpc$Global_active_power <- as.numeric(hpc$Global_active_power)
-
-#filter the date with the given value
-f_hpc <- hpc[hpc$Date == "2007-02-01" | hpc$Date == "2007-02-02" | hpc$Date == "2007-02-03" ,]
-
-#filter global active power
-#gap <-  f_hpc$Global_active_power
-
-#plot histogram
-with(f_hpc , plot( f_hpc$Date , f_hpc$Sub_metering_1 , col = "black" , ylab = "Energy sub metering" ))
-with(f_hpc , plot( f_hpc$Date , f_hpc$Sub_metering_2 , col = "red"  ,  ylab = "Energy sub metering" ))
-with(f_hpc , plot( f_hpc$Date , f_hpc$Sub_metering_3 , col = "blue" ,  ylab = "Energy sub metering" ))
-legend("topright" , legend = c("Sub_metering_1" , "Sub_metering_2" , "Sub_metering_3") , bty = "n" , lwd = 2 ,  col = c("black" , "red" , "blue") , lty = c(1,1,1) , pch = c(NA,NA,NA))
-     
-dev.copy(png , "plot3.png")
+##plot3
+## Read data from file:
+hpc <- read.csv("~/household_power_consumption.txt", sep=";")
+## Subset required data (only two dates):
+hpc2 <- subset(hpc, Date == "1/2/2007" | Date == "2/2/2007")
+## Convert date and time formats:
+hpc3 <- transform(hpc2, Date = as.Date(hpc2$Date, format = "%d/%m/%Y"))
+hpc4 <- transform(hpc3, Time = strptime(paste(hpc3$Date, hpc3$Time), 
+                                        format = "%Y-%m-%d %H:%M:%S"))
+## Define x axis:
+wds <- hpc4$Time
+## Define y axis:
+sm1 <- as.vector(hpc4$Sub_metering_1)
+sm2 <- as.vector(hpc4$Sub_metering_2)
+sm3 <- as.vector(hpc4$Sub_metering_3)
+## Create the plot:
+plot(wds, as.numeric(sm1), type = "l", xlab = "", ylab = "Energy sub metering")
+lines(wds, as.numeric(sm2), type = "l", col = "red")
+lines(wds, as.numeric(sm3), type = "l", col = "blue")
+## Add legend:
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+## Create the png file:
+dev.copy(png, file = "plot3.png")
 dev.off()
